01d7bf4f6f2a1a1eea1b71e6464882711673e729
KamiUckardes <65076967+KamiUckardes@users.noreply.github.com>
2020-05-09 18:15:12 +0300

    Add files via upload

---
+++ Plot 2 Code.R
@@ -0,0 +1,15 @@
+powerdata <- read.table("household_power_consumption.txt",skip=1,sep=";")
+
+names(powerdata) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+subpower <- subset(powerdata,powerdata$Date=="1/2/2007" | powerdata$Date =="2/2/2007")
+
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+datetime <- paste(as.Date(subpower$Date), subpower$Time)
+subpower$Datetime <- as.POSIXct(datetime)
+
+plot(subpower$Datetime,as.numeric(as.character(subpower$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+
+dev.copy(png,file="plot 2.png")
+
+dev.off()
